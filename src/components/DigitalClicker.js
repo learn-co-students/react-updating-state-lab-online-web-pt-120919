9d426db60e7164e63d1ac99615c5cc0c9ed46b9e
smakaitakori <makait87@icloud.com>
2020-09-16 19:35:39 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            timesClicked:0
+        };
+    }
+
+    handleClick = () => {
+        let actualClicks = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: actualClicks
+        })
+    };
+
+    render(){
+       return (
+            <div>
+                <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+       ) 
+    }
+
+} 
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,53 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+              }
+            }
+        };
+    }
+
+    updateBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        });
+    };
+
+    updateRes = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+            video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+            }
+            }
+        });
+    };
+
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.updateBitrate} className='bitrate' > Change bitrate</button>
+                <p>{this.state.settings.bitrate}</p>
+                <button onClick={this.updateRes} className='resolution' >Change resolution</button>
+                <p> {this.state.settings.video.resolution} </p>
+            </div>
+        )
+    }
+}
